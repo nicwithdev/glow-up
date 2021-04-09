0ba076aa7995edd8abbfea2139e117ec943f7227
Nicole WItherall <nicwitherall@gmail.com>
2021-04-09 11:07:08 -0400

    add styling to forms

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # HOME AND LOGIN
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
 
@@ -20,52 +22,27 @@ urlpatterns = [
     path('products/submit/', views.products_submit),
     path('products/<int:p_id>/delete/', views.products_delete),
 
-    #     #AM/PM Routines
-    #     path('routine/', views.routine),
-    #     path('routine/add/form/', views.routine_add),
-    #     path('routine/submit/', views.routine_submit),
-
-
-
-    # this is the user's log show page
+    # LOG PAGES
     path('log/hair/', views.hair_log),
     path('log/skin/', views.skin_log),
 
-    # these are the paths to get to the forms for the user to enter thier diary input
-    path('log/hairdiary/', views.hairdiary),
-    path('log/skindiary/', views.skindiary),
 
-    # this is the user's log' detail page of every log
+    # HAIR
     path('log/hair/<int:hair_id>/', views.hair_detail),
-
-
-    # create forms for hair diary
     path('log/hairdiary/create_form/', views.create_form),
     path('log/hairdiary/submit_create_form/', views.submit_create_form),
-
-    # delete hair diary
     path('log/<int:hair_id>/delete/', views.delete),
-
-    # update hair diary step 1: deliver form to user
     path('log/<int:hair_id>/edit/', views.edit_form),
-    # update hair diary step 2: accept form from user
     path('log/<int:h_id>/hairdiary/submit_update_form/', views.submit_update_form),
 
 
-    # create forms for skin diary
+    # SKIN
     path('log/skindiary/create_form/', views.skin_create_form),
     path('log/skindiary/submit_create_form/', views.submit_skin_form),
-
-    # this is the user's log' detail page of every log
     path('log/skin/<int:skin_id>/', views.skin_detail),
-
-    # update skin diary: edit feature
     path('log/skindiary/<int:s_id>/edit/', views.skin_edit_form),
-    # update hair diary step 2: accept form from user
     path('log/skindiary/<int:s_id>/skin_submit_update_form/',
          views.skin_submit_update_form),
-
-    # delete skin diary
     path('log/skindiary/<int:skin_id>/delete/', views.skin_delete),
 
 
